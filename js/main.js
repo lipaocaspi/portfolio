6f10cd3866f51889f48479e06d7eda52fb1f4b4d
lipaocaspi <66758443+lipaocaspi@users.noreply.github.com>
2024-08-28 21:13:58 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,139 @@
+const sidebar = document.getElementById('sidebar');
+
+const optionMenu = document.querySelectorAll('#sidebar .side-menu .option-menu');
+const contents = document.querySelectorAll('.content');
+
+function showContent(contentId) {
+	contents.forEach(content => {
+		content.classList.remove('active');
+		content.style.display = 'none';
+	});
+
+	const contentToShow = document.getElementById(contentId);
+	contentToShow.style.display = 'block';
+	setTimeout(() => {
+		contentToShow.classList.add('active');
+	}, 10);
+}
+
+optionMenu.forEach(item=> {
+	item.addEventListener('click', () => {
+		optionMenu.forEach(element => element.classList.remove('active'));
+		item.classList.add('active');
+		const contentId = item.getAttribute('data-content');
+		showContent(contentId);
+	})
+})
+
+optionMenu[0].classList.add('active');
+showContent('first-option');
+
+/* optionMenu.forEach(item=> {
+	item.addEventListener('click', () => {
+		optionMenu.forEach(element => element.classList.remove('active'));
+		item.classList.add('active');
+		contents.forEach(content=> content.style.display = 'none');
+		const contentId = item.getAttribute('data-content');
+		document.getElementById(contentId).style.display = 'block';
+	})
+	contents[0].style.display = 'block';
+}) */
+
+// SIDEBAR COLLAPSE
+const toggleSidebar = document.querySelector('nav .toggle-sidebar');
+const allSideDivider = document.querySelectorAll('#sidebar .divider');
+if(sidebar.classList.contains('hide')) {
+	allSideDivider.forEach(item=> {
+		item.textContent = '-'
+	})
+	allDropdown.forEach(item=> {
+		const a = item.parentElement.querySelector('a:first-child');
+		a.classList.remove('active');
+		item.classList.remove('show');
+	})
+} else {
+	allSideDivider.forEach(item=> {
+		item.textContent = item.dataset.text;
+	})
+}
+toggleSidebar.addEventListener('click', function () {
+	sidebar.classList.toggle('hide');
+
+	if(sidebar.classList.contains('hide')) {
+		allSideDivider.forEach(item=> {
+			item.textContent = '-'
+		})
+
+		allDropdown.forEach(item=> {
+			const a = item.parentElement.querySelector('a:first-child');
+			a.classList.remove('active');
+			item.classList.remove('show');
+		})
+	} else {
+		allSideDivider.forEach(item=> {
+			item.textContent = item.dataset.text;
+		})
+	}
+})
+sidebar.addEventListener('mouseleave', function () {
+	if(this.classList.contains('hide')) {
+		allDropdown.forEach(item=> {
+			const a = item.parentElement.querySelector('a:first-child');
+			a.classList.remove('active');
+			item.classList.remove('show');
+		})
+		allSideDivider.forEach(item=> {
+			item.textContent = '-'
+		})
+	}
+})
+sidebar.addEventListener('mouseenter', function () {
+	if(this.classList.contains('hide')) {
+		allDropdown.forEach(item=> {
+			const a = item.parentElement.querySelector('a:first-child');
+			a.classList.remove('active');
+			item.classList.remove('show');
+		})
+		allSideDivider.forEach(item=> {
+			item.textContent = item.dataset.text;
+		})
+	}
+})
+
+// MENU
+const allMenu = document.querySelectorAll('main .content-data .head .menu');
+allMenu.forEach(item=> {
+	const icon = item.querySelector('.icon');
+	const menuLink = item.querySelector('.menu-link');
+
+	icon.addEventListener('click', function () {
+		menuLink.classList.toggle('show');
+	})
+})
+window.addEventListener('click', function (e) {
+	if(e.target !== imgProfile) {
+		if(e.target !== dropdownProfile) {
+			if(dropdownProfile.classList.contains('show')) {
+				dropdownProfile.classList.remove('show');
+			}
+		}
+	}
+
+	allMenu.forEach(item=> {
+		const icon = item.querySelector('.icon');
+		const menuLink = item.querySelector('.menu-link');
+
+		if(e.target !== icon) {
+			if(e.target !== menuLink) {
+				if (menuLink.classList.contains('show')) {
+					menuLink.classList.remove('show')
+				}
+			}
+		}
+	})
+})
+// PROGRESSBAR
+const allProgress = document.querySelectorAll('main .card .progress');
+allProgress.forEach(item=> {
+	item.style.setProperty('--value', item.dataset.value)
+})
